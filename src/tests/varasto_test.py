de38e6bd7b7b1c3f0f035d5f0aba19ec46127df9
Julia <juliapoutanen@gmail.com>
2022-03-16 10:47:51 +0200

    coverage tests to 100

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -24,6 +24,19 @@ class TestVarasto(unittest.TestCase):
         # vapaata tilaa pitäisi vielä olla tilavuus-lisättävä määrä eli 2
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 2)
 
+    def test_lisays_lisaa_muuttaa_maaraa(self):
+        self.varasto.lisaa_varastoon(-1)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+        self.assertAlmostEqual(self.varasto.tilavuus, 10)
+
+        self.varasto.lisaa_varastoon(6)
+        self.assertAlmostEqual(self.varasto.saldo, 6)
+        self.assertAlmostEqual(self.varasto.tilavuus, 10)
+
+        self.varasto.lisaa_varastoon(5)
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+        self.assertAlmostEqual(self.varasto.tilavuus, 10)
+
     def test_ottaminen_palauttaa_oikean_maaran(self):
         self.varasto.lisaa_varastoon(8)
 
@@ -38,3 +51,23 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_ottaminen_muuttaa_maaraa(self):
+        self.varasto.ota_varastosta(-1)
+        result = self.varasto.ota_varastosta(1)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+        self.assertAlmostEqual(result, 0)
+
+    def test_konstruktori(self):
+        varasto = Varasto(-2, alku_saldo=5)
+        self.assertAlmostEqual(varasto.tilavuus, 0)
+        self.assertAlmostEqual(varasto.saldo, -2)
+
+        varasto = Varasto(5, alku_saldo=-1)
+        self.assertAlmostEqual(varasto.tilavuus, 5)
+        self.assertAlmostEqual(varasto.saldo, 0)
+
+    def test_objekti_str(self):
+        teksti = self.varasto.__str__()
+        self.assertAlmostEqual(teksti, 'saldo = 0, vielä tilaa 10')
